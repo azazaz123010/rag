a358ddd667f89b54c411b2ea59ac8f68450804a9
Xingyuan He <xhe19@illinois.edu>
2025-09-05 15:21:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -136,6 +136,12 @@ def profile():
         
         return render_template('profile.html', user=user)
 
+@app.route('/index')
+def index():
+    """Schools page"""
+    return render_template('index.html')
+
+
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
